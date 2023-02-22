81b315f3cf496937347f004b3efc2deca6460f99
tekuteku-tech <tekuteku.tech.mail@gmail.com>
2023-02-23 08:07:34 +0900

    新規作成

---
+++ src/main/java/com/example/demo/ApplicationRunnerImpl.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+public class ApplicationRunnerImpl implements ApplicationRunner {
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+
+        log.info("ApplicationRunner");
+
+    }
+
+}
---
+++ src/main/java/com/example/demo/WebAppTrainingApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAppTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebAppTrainingApplication.class, args);
+	}
+
+}
